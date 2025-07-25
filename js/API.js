0f59226bf8c07ec3dfd29552cf409a301b63877f
César García <cesar.garciamcs@outlook.com>
2025-07-24 19:11:35 -0600

    listando los clientes desde la API

--- js/API.js
+++ js/API.js
@@ -1,5 +1,6 @@
 const url = 'http://localhost:4000/clientes'
 
+// Cuando se crea un nuevo cliente
 export const nuevoCliente = async cliente => {
     const params = {
         method: 'POST',
@@ -13,6 +14,18 @@ export const nuevoCliente = async cliente => {
         await fetch(url, params)
 
         window.location.href = 'index.html'
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Obtiene todos los clientes
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch(url)
+        const clientes = await resultado.json()
+
+        return clientes
     } catch (error) {
         console.log(error)
     }
